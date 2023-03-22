9745df1dfe7c77a8074b66d3524c8f3f9370c744
Asbjoernemil <asbjoernemil24@gmail.com>
2023-03-22 10:25:56 +0100

    data Info Butters

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const butters = {
+  name: `Leopold "Butters" Stotch`,
+  nickname:
+    "Inspector Butters, Marjorine, Mantequilla, Paladin Butters the Merciful, Professor Chaos",
+  image:
+    "http://southparkstudios.mtvnimages.com/shared/characters/kids/butters-stotch.png",
+  occupation: "Student",
+  age: 10,
+  voicedBy: "Matt Stone",
+  gender: "Male",
+  religion: "Roman Catholic , Temporary: Blaintologist*",
+  catchPhrase: "Do you know what i am saying?",
+  hairColor: "Blond",
+  schoolGrade: 4,
+  episodes: "S01E01 to S26E05",
+  appearances: 317,
+  firstAppearance: "S01E01",
+};
